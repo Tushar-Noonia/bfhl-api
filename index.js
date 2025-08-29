21ca6b48c41cb406fddccfb904135cc4ec387f16
Tushar-Noonia <tusharnoonia123@gmail.com>
2025-08-29 11:42:47 +0530

    Initial commit - BFHL API

--- index.js
+++ index.js
@@ -1,70 +1,54 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(bodyParser.json());
 
-const FULL_NAME = "tushar_noonia";
-const DOB = "29062004";
-const EMAIL = "tusharnoonia123@gmail.com";
-const ROLL_NUMBER = "22BCE1998";
-
-function alternatingCaps(str) {
-  let result = "";
-  let toggle = true;
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += toggle ? str[i].toUpperCase() : str[i].toLowerCase();
-    toggle = !toggle;
-  }
-  return result;
-}
-
 app.post("/bfhl", (req, res) => {
-  try {
-    const data = req.body.data;
-    if (!Array.isArray(data)) {
-      return res.status(400).json({ is_success: false, message: "data must be an array" });
+  const data = req.body.data || [];
+  
+  const evenNumbers = [];
+  const oddNumbers = [];
+  const alphabets = [];
+  const specialChars = [];
+  let sum = 0;
+
+  data.forEach((item) => {
+    if (/^\d+$/.test(item)) {
+      const num = parseInt(item, 10);
+      if (num % 2 === 0) evenNumbers.push(item);
+      else oddNumbers.push(item);
+      sum += num;
+    } else if (/^[a-zA-Z]+$/.test(item)) {
+      alphabets.push(item.toUpperCase());
+    } else {
+      specialChars.push(item);
     }
+  });
 
-    let evenNumbers = [];
-    let oddNumbers = [];
-    let alphabets = [];
-    let specialChars = [];
-    let sum = 0;
+  const concatString = alphabets
+    .join("")
+    .split("")
+    .reverse()
+    .map((ch, i) => (i % 2 === 0 ? ch.toUpperCase() : ch.toLowerCase()))
+    .join("");
 
-    data.forEach(item => {
-      if (!isNaN(item)) {
-        const num = parseInt(item);
-        sum += num;
-        if (num % 2 === 0) evenNumbers.push(num);
-        else oddNumbers.push(num);
-      } else if (/^[a-zA-Z]+$/.test(item)) {
-        alphabets.push(item.toUpperCase());
-      } else {
-        specialChars.push(item);
-      }
-    });
-
-    const concatString = alternatingCaps(alphabets.join(""));
-
-    return res.status(200).json({
-      is_success: true,
-      user_id: `${FULL_NAME}_${DOB}`,
-      email: EMAIL,
-      roll_number: ROLL_NUMBER,
-      even_numbers: evenNumbers,
-      odd_numbers: oddNumbers,
-      alphabets: alphabets,
-      special_characters: specialChars,
-      sum: sum,
-      concat_string: concatString
-    });
-  } catch (err) {
-    return res.status(500).json({ is_success: false, error: err.message });
-  }
+  res.json({
+    is_success: true,
+    user_id: "tushar_noonia_29062004",
+    email: "tusharnoonia123@gmail.com",
+    roll_number: "22BCE1998",
+    even_numbers: evenNumbers,
+    odd_numbers: oddNumbers,
+    alphabets,
+    special_characters: specialChars,
+    sum,
+    concat_string: concatString,
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
